ca44c22829733da3e8fe6a5b320851a838f7e701
EMWANG97 <142074829+EMWANG97@users.noreply.github.com>
2023-08-22 13:50:05 +0800

    2D-CNN.py

--- 2D-CNN.py
+++ 2D-CNN.py
@@ -1,10 +1,10 @@
 # 2D-CNN.Developed by EM Wang / 2023.6.30 / QQ:326496053
 import csv
+import os
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D,Activation,Dropout,Flatten,Dense,AveragePooling2D,MaxPooling2D
 from tensorflow.keras.optimizers import Adam
 from keras import regularizers
-import os
 from array_data_2D import *
 from sklearn.utils import shuffle
 from tensorflow.keras.preprocessing.image import ImageDataGenerator,img_to_array,load_img
@@ -13,9 +13,9 @@ from PIL import Image
 # GPU selection
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
-
-train_dir = 'D:/faom model and analysis DL/2D analysis/2D-CNN/training_data/'
-working_path='D:/faom model and analysis DL/2D analysis/2D-CNN/'
+# initial parameters
+train_dir = 'D:/training_data/'
+working_path='D:/2D-CNN/'
 os.chdir(working_path)
 batch_size = 32
 twoD_model_size = 64
@@ -26,29 +26,28 @@ para_num = 200
 multi_model = True
 ultra_multi_model = False
 
-
 model = Sequential()
-# 64
+# size 64*64
 model.add(Conv2D(input_shape=(twoD_model_size,twoD_model_size,num_channels),filters=16,kernel_size=3,padding='same',activation='relu'))
 model.add(Conv2D(filters=16,kernel_size=3,padding='same',activation='relu'))
 model.add(AveragePooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-# 32
+# size 32*32
 model.add(Conv2D(filters=32,kernel_size=3,padding='same',activation='relu'))
 model.add(Conv2D(filters=32,kernel_size=3,padding='same',activation='relu'))
 model.add(AveragePooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-# 16
+# size 16*16
 model.add(Conv2D(filters=64,kernel_size=3,padding='same',activation='relu'))
 model.add(Conv2D(filters=64,kernel_size=3,padding='same',activation='relu'))
 model.add(AveragePooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-# 8
+# size 8*8
 model.add(Conv2D(filters=128,kernel_size=3,padding='same',activation='relu'))
 model.add(Conv2D(filters=128,kernel_size=3,padding='same',activation='relu'))
 model.add(AveragePooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-# 4
+# size 4*4
 model.add(Conv2D(filters=256,kernel_size=3,padding='same',activation='relu'))
 model.add(Conv2D(filters=256,kernel_size=3,padding='same',activation='relu'))
 model.add(AveragePooling2D(pool_size=(2, 2), strides=(2, 2)))
@@ -59,13 +58,16 @@ model.add(Dense(2048,activation='relu',kernel_regularizer=regularizers.l2(0.01))
 
 model.add(Dense(200,activation='linear'))
 
-# Optimizer
+# optimizer
 adam = Adam(learning_rate=1e-5)
+
+# compile
 model.compile(optimizer=adam,loss='mae')
 
-# File input
+# file input
 print('\n'+'-----File input-----')
 
+# basic training
 train_img=[]
 for i in range (1,data_num+1):
     img = Image.open("{}.png".format(train_dir+str(i)))
@@ -79,9 +81,12 @@ print('-----File input complete-----')
 print('The shape of batch:',model_training_data.shape)
 print('The shape of para:',para_training_data.shape)
 
+# model training by using "fit" function
 model_fit = model.fit(model_training_data, para_training_data, batch_size=batch_size, epochs=epochs, shuffle=True, validation_split=0.2)
 model.evaluate(model_training_data, para_training_data, batch_size=batch_size)
 
+# model evaluate 
+# export the training loss to csv. files
 loss_list = model_fit.history['loss']
 val_loss_list = model_fit.history['val_loss']
 loss = zip(loss_list,val_loss_list)
@@ -90,4 +95,5 @@ with open('loss.csv',"w",newline='') as loss_csv:
     for row in loss:
         writer.writerow(row)
 
+# model save
 model.save('model_2Dcnn.h5')
